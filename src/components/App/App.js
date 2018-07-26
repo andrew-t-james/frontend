4f354bdb0cbeb7fc727d97914ad25a23177fe40d
Andrew James <ajames20@gmail.com>
2018-07-26 09:37:53 -0600

    Update proptypes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes, { shape, func, string } from 'prop-types';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
@@ -8,7 +8,7 @@ class App extends Component {
   componentDidMount = async () => {
     const response = await fetch('http://localhost:3001/api/v1/houses');
     const houses = await response.json();
-    await this.props.getHouses(houses);
+    this.props.getHouses(houses);
   }
   render() {
     return (
@@ -16,10 +16,6 @@ class App extends Component {
         <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to Westeros</h2>
-          <button onClick={() => {
-            this.props.fakeAction();
-            alert(this.props.fake);
-          }}> FAKE ACTION</button>
         </div>
         <div className='Display-info'>
         </div>
@@ -29,8 +25,8 @@ class App extends Component {
 }
 
 App.propTypes = {
-  fake: shape({ fake: string }),
-  fakeAction: func.isRequired
+  houses: PropTypes.arrayOf(PropTypes.object),
+  getHouses: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
